ed093e8a114761fe81f22eb7920d0641ed844ac8
deltafunction <frederik@orellana.dk>
2021-06-17 15:57:36 +0200

    Trying again with prefix

--- jupyter_sciencedata/jupyter_sciencedata.py
+++ jupyter_sciencedata/jupyter_sciencedata.py
@@ -54,7 +54,12 @@ Context = namedtuple('Context', [
     'logger', 'multipart_uploads'
 ])
 
+# As far as I can see from
+# https://github.com/ezhov-evgeny/webdav-client-python-3/blob/871ea5f9b862553465551dd79dd5b6b298e3ff17/webdav3/client.py
+# there's no way of setting headers with webdav-client, so not much point in setting some for non-webdav methods.
+
 SCIENCEDATA_HEADERS = {};
+SCIENCEDATA_PREFIX = "files/";
 
 class ExpiringDict:
 
@@ -299,7 +304,7 @@ def _get_any(context, path, content, type, mimetype, format, decode):
 
 @gen.coroutine
 def _get_directory(context, path, content):
-    files = webdav_client.list(path, get_info=True) if content else []
+    files = webdav_client.list(SCIENCEDATA_PREFIX+path, get_info=True) if content else []
     return {
         'name': _final_path_component(path),
         'path': path,
@@ -442,7 +447,7 @@ def _delete_checkpoint(context, checkpoint_id, path):
 
 @gen.coroutine
 def _list_checkpoints(context, path):
-    files = webdav_client.list(path, get_info=True)
+    files = webdav_client.list(SCIENCEDATA_PREFIX+path, get_info=True)
     return [
         {
             'id': file['path'][(file['path'].rfind('/' + CHECKPOINT_SUFFIX + '/') + len('/' + CHECKPOINT_SUFFIX + '/')):],
@@ -545,7 +550,7 @@ def _copy(context, from_path, to_path):
 def _make_sciencedata_http_request(context, method, path, query, payload, headers):
     all_headers = {**SCIENCEDATA_HEADERS, **headers}
     querystring = urllib.parse.urlencode(query, safe='~', quote_via=urllib.parse.quote)
-    encoded_path = urllib.parse.quote(path, safe='/~')
+    encoded_path = urllib.parse.quote(SCIENCEDATA_PREFIX+path, safe='/~')
     url = f'https://sciencedata{encoded_path}' + (('?' + querystring) if querystring else '')
 
     body = \
