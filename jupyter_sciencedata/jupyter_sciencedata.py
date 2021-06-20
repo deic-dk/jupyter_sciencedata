8dcc38d017096c41d2011cc6b1809575a16e98ed
deltafunction <frederik@orellana.dk>
2021-06-20 13:43:19 +0200

    Use raw http request for move

--- jupyter_sciencedata/jupyter_sciencedata.py
+++ jupyter_sciencedata/jupyter_sciencedata.py
@@ -487,8 +487,9 @@ def _rename(context, old_path, new_path):
         raise HTTPServerError(400, "Target already exists")
 
     type = yield _type_from_path(context, old_path)
-    #response = yield _make_sciencedata_http_request(context, 'MOVE', path, {}, content_bytes, {})
-    response = yield webdav_client.move(remote_path_from=old_path, remote_path_to=new_path)
+    # webdav_client returns nothing. We need headers
+    #response = yield webdav_client.move(remote_path_from=old_path, remote_path_to=new_path)
+    response = yield _make_sciencedata_http_request(context, 'MOVE', old_path, {}, b'', {'Destination':new_path})
     last_modified_str = response.headers['Date']
     last_modified = datetime.datetime.strptime(last_modified_str, "%a, %d %b %Y %H:%M:%S GMT")
     mimestring = response.headers['Content-Type']
