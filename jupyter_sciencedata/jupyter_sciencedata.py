3752dd109f76d6c57b9c0fac045f01e26c34148d
deltafunction <frederik@orellana.dk>
2021-06-26 23:18:08 +0200

    test

--- jupyter_sciencedata/jupyter_sciencedata.py
+++ jupyter_sciencedata/jupyter_sciencedata.py
@@ -136,14 +136,13 @@ class OpCheckpoints(GenericCheckpointsMixin, Checkpoints):
         #""" -> {'type': 'notebook', 'content': <output of nbformat.read>}"""
         return (yield _get_model_at_checkpoint(self._context(), path))
 
-    def delete_checkpoint(self, checkpoint_id, path):
+#    def delete_checkpoint(self, checkpoint_id, path):
 
-        @gen.coroutine
-        def delete_checkpoint_async():
-            return (yield _delete_checkpoint(self._context(), checkpoint_id, path))
+#        @gen.coroutine
+#        def delete_checkpoint_async():
+#            return (yield _delete_checkpoint(self._context(), checkpoint_id, path))
 
-        #return _run_sync_in_new_thread(delete_checkpoint_async)
-        return []
+#        return _run_sync_in_new_thread(delete_checkpoint_async)
 
     def list_checkpoints(self, path):
         return (yield _list_checkpoints(self._context(), path))
