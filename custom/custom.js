27cbf431d0c8827b07ab99e6292c2499d3e235f9
Frederik Orellana <frederik@orellana.dk>
2025-10-21 15:39:10 +0200

    non-minified jquery

--- custom/custom.js
+++ custom/custom.js
@@ -87,7 +87,7 @@
   // Load the script
   const script = document.createElement("script");
   // 3.6.0https://sciencedata.dk/shared/kubefiles_public
-  script.src = 'https://sciencedata.dk/public/kubefiles_shared/jupyter/jquery.min.js';
+  script.src = 'https://sciencedata.dk/public/kubefiles_shared/jupyter/jquery-3.7.1.js';
   script.type = 'text/javascript';
   script.addEventListener('load', () => {
     console.log(`jQuery ${$.fn.jquery} has been loaded successfully!`);
